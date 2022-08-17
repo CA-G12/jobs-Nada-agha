757bde58d2f0b52711122187701fa639660b92cd
Mohammed alagha <modyagha7@gmail.com>
2022-08-17 13:43:05 +0300

    handel the server error relates #23

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,5 +1,12 @@
 let btn =document.querySelector('button');
 let input = document.querySelector('input')
 btn.addEventListener('click',(e)=>{
-    fetchNode('/search',{q:input.value}).then(data=>data.json()).then(console.log);
+    fetchNode('/search',{q:input.value}).then((data)=>{
+        if(data.status==500){
+            window.location.href='../errors/500.html'
+        }
+        else{
+            data.json().then(console.log)
+        }
+    });
 })
