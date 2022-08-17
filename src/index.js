ac87e37c7368afd3c0c4f580ac21efe6d931faa6
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2022-08-17 08:22:35 +0300

    create erver side relates #10

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ const path = require("path")
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000;
-const fetch = require('node-fetch');
-app.use(express.static(path.join(__dirname, '..', 'public')))
+const fetch= require("node-fetch")
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(express.urlencoded({ extended: false }));
-
-app.post('/search', (req, res) => {
-    let name = req.body.q
-    fetch(`https://api.tvmaze.com/search/shows?q=${name}`)
-        .then(res => res.json())
-        .then(text => res.send(text));
-
+app.post("/search",(req,res)=>{
+ let name=req.body.q
+ fetch(`https://api.tvmaze.com/search/shows?q=${name}`).then
+ (data=>data.json())
+ .then(result =>{
+     return res.send(result);
+ })
 })
-
 app.listen(port, () => {
     console.log("server is listening")
 })
