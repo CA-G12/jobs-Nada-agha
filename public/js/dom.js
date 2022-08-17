f9cc621ca09ecb06a5d3ba98dd0140e3371cc080
Mohammed alagha <modyagha7@gmail.com>
2022-08-17 14:53:28 +0300

    handle the data returned from the api and pass it to the dom related #26

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,12 +1,13 @@
 let btn =document.querySelector('button');
 let input = document.querySelector('input')
+let jobsContainer=document.querySelector('.static-jobs')
 btn.addEventListener('click',(e)=>{
     fetchNode('/search',{q:input.value}).then((data)=>{
         if(data.status==500){
             window.location.href='../errors/500.html'
         }
         else{
-            data.json().then(console.log)
+            data.json().then(data=>generateJopCards(data,jobsContainer))
         }
     });
 })
